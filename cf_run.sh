def0bdc650f95761748257ccbd05cb49b36b9732
luan.venancio <luan.venancio.ext@tlscontact.com>
2023-09-11 23:15:25 +0100

    chore: added connection pooling and implemented pagination, both will help the concurrency problem

--- cf_run.sh
+++ cf_run.sh
@@ -7,31 +7,19 @@ export AMQP_URI=$(echo ${VCAP_SERVICES} | jq -r '.rabbitmq[0].credentials.uri')
 export POSTGRES_URI=$(echo ${VCAP_SERVICES} | jq -r '.postgresql[0].credentials.uri')/devex
 export REDIS_URI=$(echo ${VCAP_SERVICES} | jq -r '.redis[0].credentials.uri')
 
-# Create dbname for PostgreSQL
+# create dbname for postgres
+
 python -c """
 import psycopg2 as db
-from psycopg2 import pool
 from urllib.parse import urlparse
-
 result = urlparse('${POSTGRES_URI}')
 username = result.username
 password = result.password
 database = result.path[1:]
 hostname = result.hostname
 port = result.port
-
-connection_pool = pool.SimpleConnectionPool(
-    minconn=1,
-    maxconn=10,
-    dbname=database,
-    user=username,
-    password=password,
-    host=hostname,
-    port=port
-)
-
-import os
-os.environ['DATABASE_CONNECTION_POOL'] = connection_pool
+con=db.connect(dbname='postgres',host=hostname,user=username,password=password)
+con.autocommit=True;con.cursor().execute('CREATE DATABASE devex')
 """
 
-./run.sh $@
+./run.sh $@ 
